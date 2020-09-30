e9d029bda6f6721d7d0df63726ccf80efd3b84f9
Sara A <calisaraalhindi@gmail.com>
2020-09-30 17:40:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,5 +45,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) as book_count from character_books 
   JOIN characters 
   ON character_books.character_id = characters.id
-  GROUP BY characters.name"
+  GROUP BY characters.name
+  ORDER BY book_count"
 end
