36954f2c8ed94812f34dc92ca88c2a523dfb6651
Sara A <calisaraalhindi@gmail.com>
2020-09-30 17:21:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
   JOIN books
   ON books.series_id = series.id
   JOIN character_books.book_id = books.id
-  WHERE characters.species = 'human'"
+  WHERE characters.species = human"
 end
 
 def select_character_names_and_number_of_books_they_are_in
